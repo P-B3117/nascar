0304351355d5f8298dd426ddd232324b09b2290c
alexlechass <leca1403@usherbrooke.ca>
2023-10-26 14:24:30 -0400

    premiere etape du premiere tour vert

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 //variable globale couleur
 int couleurInitiale = 0;
 
+//variable algo
+int etape = 1;
 void setup()
 {
   BoardInit();
@@ -34,23 +36,39 @@ void loop()
 // Serial.println(detection_distance_bas());
   switch(couleurInitiale){
     case VERT:
-   
-    avance();
-    if (detection_distance_haut()>120){
-      Serial.println("****************************************************************************************************************");
-      slowDown();
+
+      switch(etape){
+      
+      case 1: //ligne depart avance jusqua fin mur
+        avance();
+        if (detection_distance_haut()>120){
+          etape ++; 
+        }
+        break;
+        
+      case 2://on tourne
+        slowDown();// pour tourne droite
+        if (detection_distance_haut()<120){
+          etape ++; 
+        }
+        break;
+       
+      case 3:// avance tapis
+        avance();
+        if (detection_distance_haut()>120){
+          etape ++; 
+        }
+        break;
+      case 4://on tourne encore
+        slowDown();// pour tourne droite
+        etape ++;
+        break;      
+
     }
-   
-    break;
-  
-
-    case JAUNE: //Parcour a effectuer couleur initiale jaune
+      break;
     
+
+    case JAUNE: //Parcour a effectuer couleur initiale jaune  
     break;
-  }
-
-
-  //tournegauche(lecture_gauche, lecture_droite, vitesse_tournegauche_gauche,vitesse_tournegauche_droite);
-  //tournedroit(lecture_gauche, lecture_droite, vitesse_tournedroite_gauche, vitesse_tournedroite_droite);
-
-}
+    }
+    }
