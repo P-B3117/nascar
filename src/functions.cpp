612f8d505b56c0e2048748b213324c0025567fb5
alexlechass <leca1403@usherbrooke.ca>
2023-10-26 11:56:16 -0400

    changer return de place

--- src/functions.cpp
+++ src/functions.cpp
@@ -14,41 +14,49 @@ int suiveur_ligne()
         int option=AUCUN;
         Serial.println("aucun   ");
         Serial.println(option);
+        return option;
+
     }
     else if(tension_suiveur > 2.3 && tension_suiveur<3){
         int option=GAUCHE;
         Serial.println("gauche   ");
         Serial.println(option);
+        return option;
     }
     else if(tension_suiveur>1 && tension_suiveur<1.5){
          int option=CENTRE;
         Serial.println("centre   ");
         Serial.println(option);
+        return option;
     }
     else if (tension_suiveur>0.3 && tension_suiveur<1){
          int option=DROITE;
         Serial.println("droite   ");
         Serial.println(option);
+        return option;
     }
     else if(tension_suiveur>3.5 && tension_suiveur<4.3){
          int option=GAUCHEETCENTRE;
         Serial.println("gauche et centre   ");
         Serial.println(option);
+        return option;
     }
     else if (tension_suiveur>3 && tension_suiveur<3.5){
          int option=GAUCHEETDROITE;
         Serial.println("gauche et droite   ");
         Serial.println(option);
+        return option;
     }
     else if (tension_suiveur>1.5 && tension_suiveur<2.3){
          int option=CENTREETDROITE;
         Serial.println("centre et droite  ");
         Serial.println(option);
+        return option;
     }
     else if(tension_suiveur>4.3){
          int option=TOUT;
         Serial.println("tout   ");
         Serial.println(option);
+        return option;
     }
-    return option;
 }
