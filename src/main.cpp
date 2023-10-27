d9cd92d13cea45a028b554a1b6bf55e6c8dd8c90
alexlechass <leca1403@usherbrooke.ca>
2023-10-27 14:25:13 -0400

    avance jusquau blanc

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ void loop()
  //Serial.println(detectionSifflet());
  //Serial.println(detection_distance_haut());
   switch(couleurInitiale){
-    case JAUNE:
+    case BLEU:
 
       switch(etape){      
       case 1: //ligne depart avance jusqua fin mur
@@ -62,31 +62,40 @@ void loop()
       
       
         
-      case 3:// avance a coter poutre jusqua verre
+      case 3:// avance a coter poutre jusquau verre et arrete d'avancer sur plan
         computePIDLigneDroite(3200,3200,0.3,0.305);//enttre ligne droite dans le futur
         Serial.println("j'avance");
         if (detection_distance_haut()<20){
+          Serial.println("servo!!!!!");
+          SERVO_SetAngle(1,40);
+          delay(20); 
+          computePIDLigneDroite(3200,3200,0.3,0.305);
+          if (getCouleur()==BLANC){
           etape ++;
+          }
         }
+        else if (getCouleur()==BLANC){
+          etape++;
+        }
+        
         break;
       
-      case 4:
-      Serial.println("servo!!!!!");
-      SERVO_SetAngle(1,40);
-      delay(20);
-      computePIDLigneDroite(3200,3200,0.3,0.305);
+      /*case 4:
+      
+     
       if (getCouleur()==BLANC){
         etape ++;
       }
       break;
-
-      case 5:
+*/
+      case 4:
+      Serial.print("case 4");
       computePID(0,0,0,0);
       break;
-     }
+      }
     case VERT: //Parcour a effectuer couleur initiale jaune  
     
     break;
-   
+      
   }
-}
+  }
