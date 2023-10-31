92a3c22ecc7c0a7540bdcec0c6c589b80ea10a99
alexlechass <leca1403@usherbrooke.ca>
2023-10-30 20:47:33 -0400

    Update functions.cpp

--- src/functions.cpp
+++ src/functions.cpp
@@ -4,7 +4,82 @@
 #include "ArduPID.h"
 
 //définir les fonctions ici
+void computePID(int targetDroit, int targetGauche, float pwrLimitDroit, float pwrLimitGauche){
 
+  
+  // PID constants Droite
+  float kpDroite = 0.000999;
+  float kdDroite = 0.000000001;
+  float kiDroite = 0.0;
+
+  float kpGauche = 0.000999;
+  float kdGauche = 0.000000001;
+  float kiGauche = 0.0;
+
+  //time difference
+  long currT = micros();
+
+  float deltaT = ((float)(currT-prevT))/1.0e6;
+  prevT = currT; 
+
+  // set target position
+  setTarget(currT/1.0e6,deltaT, targetDroit, targetGauche);
+
+  int posDroite = ENCODER_Read(Droite);
+  int posGauche = ENCODER_Read(Gauche);
+
+  // erreur
+  int eDroite = target[0] - posDroite;
+  int eGauche = target[1] - posGauche;
+
+  //derive
+  float dedtDroite = (eDroite-eprevDroite)/(deltaT);
+  float dedtGauche = (eGauche-eprevGauche)/(deltaT);
+
+  //integral
+  eintegralDroite = eintegralDroite + eDroite*deltaT;
+  eintegralGauche = eintegralGauche + eGauche*deltaT;
+
+  //control signal
+  float uDroite = kpDroite*eDroite + kdDroite*dedtDroite + kiDroite*eintegralDroite;
+  float uGauche = kpGauche*eGauche + kdGauche*dedtGauche + kiGauche*eintegralGauche;
+
+  // moteur power
+  float pwrDroite = fabs(uDroite);
+  float pwrGauche = fabs(uGauche);
+  if(pwrDroite>pwrLimitDroit){
+
+    pwrDroite=pwrLimitDroit;
+    if(targetDroit<0){
+      pwrDroite = pwrDroite*-1;
+    }
+  }
+  if(pwrGauche>pwrLimitGauche){
+
+    pwrGauche=pwrLimitGauche;
+    if(targetGauche<0){
+      pwrGauche = pwrGauche*-1;
+    }
+  }
+
+  //Signal to motor
+  //il faut ajouter une -Valeur a la condition pour arriver au valeurs pile que nous voulons
+  if(posDroite>=targetDroit){
+    MOTOR_SetSpeed(Droite,0);
+  }
+  else{
+    MOTOR_SetSpeed(Droite,pwrDroite);
+  }
+  if(posGauche>=targetGauche){
+    MOTOR_SetSpeed(Gauche,0);
+  }
+  else{
+    MOTOR_SetSpeed(Gauche,pwrGauche);
+  }
+  //store previous error
+  eprevDroite = eDroite;
+  eprevGauche = eGauche;
+}
 int detecteur_ligne()
 {
     int option;
